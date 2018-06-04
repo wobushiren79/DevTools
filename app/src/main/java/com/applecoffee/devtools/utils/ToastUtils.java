201222d35fe5fd6995b9e859ddecf0a44e9b970e
0.0 <734719526@qq.com>
2018-06-04 11:29:38 +0800

    增加Toast居中

--- app/src/main/java/com/applecoffee/devtools/utils/ToastUtils.java
+++ app/src/main/java/com/applecoffee/devtools/utils/ToastUtils.java
@@ -1,6 +1,7 @@
 package com.applecoffee.devtools.utils;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.Toast;
 
 /**
@@ -11,6 +12,7 @@ public class ToastUtils {
 
     /**
      * Toase  短时间
+     *
      * @param context
      * @param message
      */
@@ -20,10 +22,36 @@ public class ToastUtils {
 
     /**
      * Toast 长时间
+     *
      * @param context
      * @param message
      */
     public static void showToastLong(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+
+    /**
+     * Toase  短时间 居中
+     *
+     * @param context
+     * @param text
+     */
+    public static void showToastShortInCenter(Context context, String text) {
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    /**
+     * Toast 长时间 居中
+     *
+     * @param context
+     * @param text
+     */
+    public static void showToastLongInCenter(Context context, String text) {
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
